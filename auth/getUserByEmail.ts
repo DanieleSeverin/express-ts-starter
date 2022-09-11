f359f63d4dd66fbfb238187eff10e7c05164fd6c
DanieleSeverin89 <daniele.severin@hbtsa.it>
2022-09-11 21:09:27 +0200

    fix login, update gitignore

--- auth/getUserByEmail.ts
+++ auth/getUserByEmail.ts
@@ -1,9 +1,11 @@
 import * as mongoDB from 'mongodb';
+import { WithId } from 'mongodb';
 import { logError } from '../logger/logErrors';
+import { User } from '../models/Auth';
 const { MongoClient } = mongoDB;
 
 //Get user by email
-export async function getUserByEmail(email: string){
+export async function getUserByEmail(email: string) :Promise<User | null>{
     const url = process.env.MONGODB_URL!;
     const client = new MongoClient(url);
     const dbName = process.env.MONGODB_NAME;
@@ -13,8 +15,14 @@ export async function getUserByEmail(email: string){
         const db = client.db(dbName);
         const collection = db.collection('users');
         const query = { email: email };
-        const user = await collection.findOne(query);
-        return user;
+        const document :WithId<mongoDB.Document> | null = await collection.findOne(query);
+        const user :User = {
+            id: document?._id.toString(),
+            email: document?.email,
+            password: document?.password
+        } 
+        
+        return document ? user : null;
     } catch (err :any) {
         console.log(err.stack);
         logError(err);
--- auth/login.ts
+++ auth/login.ts
@@ -1,18 +1,26 @@
 import express from 'express';
 import { LoginResponse } from '../models/Auth';
+import { getUserByEmail } from './getUserByEmail';
 import { generateAccessToken, generateRefreshToken } from './jwt';
 const router = express.Router();
 
 router.post('/', async function(req, res) {
     console.log("Login Request");
+    const existingUser = await getUserByEmail(req.body.email);
+    if(!existingUser || existingUser.password !== req.body.password){
+        console.log("Invalid email or password: ", existingUser, req.body);
+        res.status(400).send({message: "Access denied."});
+        return;
+    }
+
     const response :LoginResponse = {
         user: {
-            id: "1",
-            name: "John Doe",
-            email: "boh"
+            id: existingUser.id,
+            email: existingUser.email,
+            password: existingUser.password
         },
-        accessToken: generateAccessToken("1"),
-        refreshToken: generateRefreshToken("1")
+        accessToken: generateAccessToken(existingUser.id!),
+        refreshToken: generateRefreshToken(existingUser.id!)
     } 
     res.send(response);
 });
